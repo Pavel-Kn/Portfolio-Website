f709e5a5474ebb0f31a7b47cad4831a731dbe64b
Pavel-Kn <ferrari.inside@gmail.com>
2022-04-08 11:38:41 +0300

    add change themes colors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controllers');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('.main-content')
+const allSections = document.querySelector('.main-content');
 
 function PageTransitions(){
     // Button click active class
@@ -12,6 +12,24 @@ function PageTransitions(){
             this.className += ' active-btn';
         })
     }
+
+    //Sections active class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+            //hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })   
 }
 
 PageTransitions();
