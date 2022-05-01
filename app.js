37bdb099f1cb4bc121c1bacacbfd6376a741ae05
PavelKn <ferrari.inside@gmail.com>
2022-05-01 16:39:38 +0300

    complete theme switcher

--- app.js
+++ app.js
@@ -29,7 +29,15 @@ function PageTransitions(){
             const element = document.getElementById(id);
             element.classList.add('active');
         }
-    })   
+    }) 
+    
+    //toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',()=>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
+
 }
 
 PageTransitions();
